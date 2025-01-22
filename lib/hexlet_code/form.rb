c8c8d85f2c6e3163b0836d50fa8d29748c08b91c
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2025-01-22 22:35:03 +0300

    step 4: add class Form

--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -1,8 +1,14 @@
 module HexletCode
-  def self.form_for(_object, attributes = {})
+  class Form
+    attr_accessor :tag
+  end
+
+  def self.form_for(_object, attributes = {}, &_block)
     attributes[:action] = attributes.delete(:url) if attributes.key?(:url)
     default_form_attributes = { action: '#', method: 'post' }
     default_form_attributes.merge!(attributes)
-    HexletCode::Tag.build('form', default_form_attributes) {}
+    form = Form.new
+    form.tag = HexletCode::Tag.build('form', default_form_attributes) {}
+    yield form
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -14,16 +14,22 @@ class TestHexletCode < Minitest::Test
 
   def test_form_by_struct
     expected = '<form action="#" method="post"></form>'
-    assert ::HexletCode.form_for(@user) == expected
+    HexletCode.form_for(@user) do |f|
+      assert f.tag == expected
+    end
   end
 
   def test_form_by_struct_and_attribute_class
     expected = '<form action="#" method="post" class="hexlet-form"></form>'
-    assert ::HexletCode.form_for(@user, class: 'hexlet-form') == expected
+    HexletCode.form_for(@user, class: 'hexlet-form') do |f|
+      assert f.tag == expected
+    end
   end
 
   def test_form_by_struct_and_attribute_url
     expected = '<form action="/profile" method="post" class="hexlet-form"></form>'
-    assert ::HexletCode.form_for(@user, url: '/profile', class: 'hexlet-form') == expected
+    HexletCode.form_for(@user, url: '/profile', class: 'hexlet-form') do |f|
+      assert f.tag == expected
+    end
   end
 end
