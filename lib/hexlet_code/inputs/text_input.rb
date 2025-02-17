5829fa65592d2550576b584e0493756f22fbda63
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2025-02-17 14:48:29 +0300

    default numbers to constants

--- lib/hexlet_code/inputs/text_input.rb
+++ lib/hexlet_code/inputs/text_input.rb
@@ -5,12 +5,16 @@ module HexletCode
     class TextInput < BaseInput
       attr_accessor :value, :cols, :rows, :attributes
 
+      DEFAULT_COUNT_OF_ROWS = 40
+      DEFAULT_COUNT_OF_COLS = 20
+      private_constant :DEFAULT_COUNT_OF_ROWS, :DEFAULT_COUNT_OF_COLS
+
       def initialize(name, value, attributes = {})
         super()
         @name = name
         @value = value
-        @cols = attributes[:cols] ||= 20
-        @rows = attributes[:rows] ||= 40
+        @cols = attributes[:cols] ||= DEFAULT_COUNT_OF_COLS
+        @rows = attributes[:rows] ||= DEFAULT_COUNT_OF_ROWS
         @attributes = attributes
       end
     end
